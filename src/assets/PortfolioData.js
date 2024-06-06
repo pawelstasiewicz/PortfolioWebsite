5754058a700b3ee8733b0a1271f00ba7782c33be
pawelstasiewicz <pst357357@gmail.com>
2024-06-06 20:59:56 +0200

    First commit

---
+++ src/assets/PortfolioData.js
@@ -0,0 +1,47 @@
+import { AgeCalculator } from './img';
+import { CarWebsite } from './img';
+import { LuckyShrub } from './img';
+
+const portfolioData = {
+	nav: ['Paweł Stasiewicz', 'About Me', 'Projects', 'Contact', 'IMG!!!'],
+	header: [
+		"Hello, I'm",
+		'Paweł Stasiewicz',
+		'Junior Frontend Developer',
+		'IMG!!!',
+		'IMG!!!',
+		'IMG!!!',
+		'IMG!!!',
+		'Download CV',
+	],
+	sectionAbout: [
+		'Education',
+		'2022 - 2023',
+		'Postgraduate Studies - Collegium Da Vinci in Poznań',
+		'Frontend Developer course of study',
+		'2017 - 2020',
+		'State Higher Vocational School in Koszalin',
+		'National Security course of study',
+		'I have completed postgraduate studies in frontend development and I am currently seeking my first job in the IT industry. I have dedicated a lot of time to studying programming materials, which has turned it into my main hobby. I am willing to constantly expand my knowledge and learn new technologies.',
+	],
+	sectionProects: [
+		LuckyShrub,
+		'Lucky Shrub',
+		CarWebsite,
+		'Car Website',
+		AgeCalculator,
+		'Age Calculator',
+		'GitHub',
+		'Live Demo',
+	],
+	sectionContact: ['pst357357@gmail.com', 'LinkedIn'],
+};
+
+const portfolioColor = {
+	hexGray1: '#303030', // color for nav elements, h1 text in header, icon in header, section title, names of schools, text in about me, borders, names of projects, text in projects buttons, all elements in contact
+	hexGray2: '#4f4f4f', // color in header h2 elements, button text in header, education in about me
+	hexGray3: '#868686', // color date in education
+	hexWhite: '#fbfbfb', //background color
+};
+
+export default { portfolioColor, portfolioData };
