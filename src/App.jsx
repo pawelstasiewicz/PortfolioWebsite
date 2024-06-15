346cddc21dca0c856fa2b2ed827bb6ce62ee6cec
pawelstasiewicz <pst357357@gmail.com>
2024-06-15 21:54:56 +0200

    Finish adding content to About me section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { useEffect } from 'react'
 import './App.css'
 import LoadingScreen from './components/organisms/LoadingScreen/LoadingScreen'
 import PortfolioPage from './components/organisms/PortfolioPage/PortfolioPage'
 
-function App() {
+const App = () => {
+
+  useEffect(() =>{
+    const timer = setTimeout(() =>{
+      window.scrollTo(0,0);
+    }, 10);
+
+    return () => clearTimeout(timer)
+
+  }, []);
 
   return (
     <>
---
+++ src/components/atoms/Sections/StyledAboutMeText.styled.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import { portfolioColor } from "../../../assets/PortfolioData";
+
+export const StyledAboutMeText = styled.div`
+    border: 1px solid ${portfolioColor.hexGray1};
+    border-radius: 32px;
+    width: 70%;
+    margin: 60px auto;
+    text-align: justify;
+`
