a820f9c54a85ac2deb81283541aeebf4be35524f
pawelstasiewicz <pst357357@gmail.com>
2024-06-16 17:57:41 +0200

    Create Projects section on mobile

---
+++ src/components/atoms/Sections/StyledCardProject.styled.jsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+import { portfolioColor } from "../../../assets/PortfolioData";
+
+export const StyledCadrProject = styled.div`
+    width: 70%;
+    border: 1px solid ${portfolioColor.hexGray1};
+    border-radius: 21px;
+    margin: 60px auto;
+    text-align: center;
+
+    img{
+        width: 118px;
+        height: 142px;
+        border-radius: 15px; 
+        margin: 20px 30px;
+    }
+
+    p{
+        font-family: 'Inika'
+    }
+
+    div{
+
+        margin: 20px 0;
+
+        button{
+            border: 1px solid ${portfolioColor.hexGray1};
+            border-radius: 21px;
+            height: 25px;
+            margin: 5px;
+            background-color: transparent;
+
+            a{
+                color: ${portfolioColor.hexGray1};
+                text-decoration: none;
+                margin: 25px 15px;
+                font-family: 'Inika';
+            }
+        }
+    }
+`
---
+++ src/components/molecules/SectionProjects/StyledProjects.styled.jsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const StyledProjects = styled.div`
+    position: relative;
+    top: 100px;
+`
